fa7e522eb2d6bf3a9ef93e8dc92afcaa8753d7b0
Zhaoyang <yiz24@uw.edu>
2017-02-22 10:53:18 -0800

    finished

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,4 @@
 'use strict';
-import ip = require('ip');
 
 //make the client
 const net = require('net'),
@@ -9,22 +8,44 @@ const net = require('net'),
     
 
 client.on('data', function(data) { //when we get data
-    
+    console.log("Received: "+data + '\n'); //output it
 });
 
 client.on('close', function() { //when connection closed
-    
+    console.log('server disconnected');
+    console.log('closing client');
+    process.exit(0);
 });
 
 
-var HOST = ip.address();
+var HOST = '127.0.0.1';
 var PORT = 3000;
 
 //connect to the server
 client.connect(PORT, HOST, function() {
+    console.log('Connected to: ' + HOST + ':' + PORT);
+    
+    // prompt the user
+    io.setPrompt('> ');
+    io.prompt();
 
+    io.on('line', function(line){
+        switch(line.trim()) {
+        case 'exit':
+            client.end();
+            console.log('client disconnected');
+            process.exit(0);
+            break;
+        default:
+            client.write(line);
+            break;
+        }
+        // then prompt again
+        io.prompt();
+    }).on('close', function() {
+        //end the connection to the server if the user manually closes
+        client.end();
+        console.log('client disconnected');
+        process.exit(0);
+    });
 });
-
-
-
-
--- src/server.ts
+++ src/server.ts
@@ -7,18 +7,60 @@ interface Address { port: number; family: string; address: string; };
 // create socket server
 let server:net.Server = net.createServer();
 
-// when the server is connected
-server.on('connection', function(socket:net.Socket){
+// array of the connected clients
+let clients: net.Socket[] = [];
 
-    // when data is sent to the socket
+server.on('connection', function(socket:net.Socket){
+    // helper function to broadcast to all other clients
+    function broadcast(name:String, message:String){
+        clients.forEach(function(client:net.Socket) {
+            //don't send to self
+            if (client !== socket) {
+                client.write('[' + name + '] ' + message + '\n');
+            }
+        });
+    }
+
+    //we've established a socket to use
+    // add it to the list of sockets
+    console.log('connected :' + socket.remoteAddress);
+    clients.push(socket);
+
+    // write a message to the client that just connected
+    socket.write("Hi what's your name? \n");
+    let name: String = '';
+
     socket.on('data', function(data){
-        //
+        let message:String = data.toString();
+        if(message.length === 0){
+            socket.write('(type something and hit return)\n');
+            return;
+        }
+
+        //if we haven't captured the name yet, treat this as the name
+        if (!name) {
+            //truncate name to 10 characters, just to be safe
+            name = data.toString().substr(0, 10);
+
+            //respond with a friendly greeting
+            socket.write('Hello ' + name + '!\n');
+            socket.write('Welcome to the chat room, ' + name + '!\n');
+            socket.write('There are ' + clients.length + ' people here.\n');
+            socket.write("Type messages, or type 'exit' at any time to leave.\n");
+        }
+        else {
+            //if message is exactly 'exit' then close the connection
+            if ('exit' === message) {
+                socket.end();
+            }
+            else {
+                //broadcast the message to all other clients
+                broadcast(name, message);
+            }
+        }
     });
 
-    socket.on('close', function(){
-        // handle client disconnecting
-    })
-
+    
 
 });
 
@@ -26,10 +68,12 @@ server.on('connection', function(socket:net.Socket){
 server.on('listening', function() {
     //output to the console the port number on which we are listening
     var addr:Address = server.address();
+    console.log(addr.address);
     console.log('server listening on port %d', addr.port);
 });
 
 //start the server
+// server.listen(3000);
 server.listen({
   host: ip.address(),
   port: 3000
